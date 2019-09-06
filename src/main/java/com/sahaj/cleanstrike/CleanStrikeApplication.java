063d56e9a863c0f6b0406c016fdeac836d13ca0c
Sameer Neema <sameer.neema02@gmail.com>
2019-09-06 10:02:38 +0530

    initial commit

---
+++ src/main/java/com/sahaj/cleanstrike/CleanStrikeApplication.java
@@ -0,0 +1,13 @@
+package com.sahaj.cleanstrike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanStrikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanStrikeApplication.class, args);
+	}
+
+}
